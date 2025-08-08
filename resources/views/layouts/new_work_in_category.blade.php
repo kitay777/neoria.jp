6b0cf9b5638ae3dd66114af67dfa5b9fc794f38e
kitayama <kitayama@main.co.jp>
2025-08-08 15:13:35 +0900

    自分の登録した情報のみを見れるようにした

---
+++ resources/views/layouts/new_work_in_category.blade.php
@@ -0,0 +1,11 @@
+<p>{{ $user->name }}さん、こんにちは。</p>
+
+<p>あなたが出品しているカテゴリ「{{ $work->category->name ?? '未分類' }}」に、新しい仕事が登録されました。</p>
+
+<p><strong>{{ $work->title }}</strong></p>
+<p>納期：{{ $work->execution_date ?? '未設定' }}</p>
+<p>詳細：{{ Str::limit($work->description, 80) }}</p>
+
+<p>
+    <a href="{{ route('works.show', $work) }}">この仕事を見る</a>
+</p>
